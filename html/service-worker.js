4f195470923fa5b285cef991e41aecd731ae233f
FUS-37 <zyf1253607602@163.com>
2024-09-26 01:32:44 +0800

    Site updated: 2024-09-26 01:32:44

--- html/service-worker.js
+++ html/service-worker.js
@@ -1,10 +1,13 @@
 const CACHE_NAME = 'my-cache-v1';
 const urlsToCache = [
     '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/image.jpg'
+    'https://w3svwsauq3bn5lj.oss-cn-beijing.aliyuncs.com/img/WIINTER.png',
+    'https://w3svwsauq3bn5lj.oss-cn-beijing.aliyuncs.com/mp3/winter.mp3',
+    'https://w3svwsauq3bn5lj.oss-cn-beijing.aliyuncs.com/img/hydrangea%20haze.png',
+    'https://w3svwsauq3bn5lj.oss-cn-beijing.aliyuncs.com/mp3/hydrangea%20haze.mp3',
+    'https://w3svwsauq3bn5lj.oss-cn-beijing.aliyuncs.com/img/%E9%9D%92%E5%B2%9A.png',
+    'https://w3svwsauq3bn5lj.oss-cn-beijing.aliyuncs.com/mp3/%E9%9D%92%E5%B2%9A.mp3',
+    './styles.css',
 ];
 
 self.addEventListener('install', event => {
